0c85639edfd227c69ab22a86def15ddfcde9cc76
GironaLE <ferreteriagirona@laespecialista.es>
2026-07-27 12:34:22 +0200

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -46,11 +46,9 @@ export default function Sidebar({
   return (
     <aside className="bg-slate-900 text-white w-full md:w-[420px] md:min-h-screen">
       <div className="p-6 border-b border-slate-700">
-      <img
-  src={logo}
-  alt="La Especialista"
-  className="w-full max-w-[260px] mx-auto mb-4"
-/>
+    <div className="bg-red-500 text-white p-4">
+  PRUEBA SIDEBAR
+</div>
 
         <p className="text-sm text-slate-400 text-center">
           Suite Profesional PYL
